9b921bbbc253b58f5c4eb1ae4d4a8eae65a8c3b7
Anthony Tran <atran.dummy@gmail.com>
2014-10-06 14:14:58 -0500

    some prompt updates

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -45,7 +45,7 @@ function fish_prompt
     if [ (_is_git_dirty) ]
       set git_info $red $git_branch " ✘"
     else
-      set git_info $green $git_branch " ⚡"
+      set git_info $green $git_branch
     end
     echo -n -s ' ' $git_info $normal
   end
